bad2f8dddfd9f4a3a6bd87a76d529580c33d41fe
Snacks <gwen@winterroot.net>
2012-03-01 19:01:08 -0500

    finished!

--- application/views/header_public.php
+++ application/views/header_public.php
@@ -9,10 +9,18 @@
 <div id="navmenu" class="grid_6 push_1">
 <div class="nav">
 <ul>
-<li><a href="<?=url::base();?>artists"  class="<?echo latticeview::withinSubtree('artists') ? "active" : "";?>" >Artists</a></li>
-  <ul>
-    <li><a href="#"> test</a></li>
+<li><a href="<?=url::base();?>artists"  class="<?echo latticeview::withinSubtree('artists') ? "active" : "";?>" >Artists</a>
+  <ul class="show">
+<div class="representedDrop">
+    <li><a href="#"> test</a></li><br/>
+    <li><a href="#">test two</a></li><br/>
+</div>
+<div class="worksAvailDrop">
+    <li><a href="#">test 3</a></li><br/>
+    <li><a href="#">test4</a></li><br/>
+</div>
   </ul>
+</li>
 <li><a href="<?=url::base();?>news"  class="<?echo latticeview::withinSubtree('news') ? "active" : "";?>" >News</a></li>
 <li><a href="<?=url::base();?>press"  class="<?echo latticeview::withinSubtree('press') ? "active" : "";?>" >Press</a></li>
 <li><a href="<?=url::base();?>exhibitions"  class="<?echo latticeview::withinSubtree('exhibitions') ? "active" : "";?>" >Exhibitions</a></li>
