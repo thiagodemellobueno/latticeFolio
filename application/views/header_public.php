2379c1e2a650da797cb2ba2804b0533246eff89b
thiago demello bueno <thiago@madeofpeople.org>
2012-03-03 14:16:40 -0600

    changes from staging, about to switch jcacciola user to read only repo

--- application/views/header_public.php
+++ application/views/header_public.php
@@ -1,24 +1,56 @@
 <div class="container_12">
 <div class="header">
 <div id="masthead" class="grid_6 alpha">
-<h2 id="logo"><a href="<?=url::base();?>home">J. Cacciola Gallery</a></h2>
-
-      </div>
+	<h2 id="logo"><a href="<?=url::base();?>home">J. Cacciola Gallery</a></h2>
+</div>
 
 
 <div id="navmenu" class="grid_6 push_1">
 <div class="nav">
 <ul>
 <li><a href="<?=url::base();?>artists"  class="<?echo latticeview::withinSubtree('artists') ? "active" : "";?>" >Artists</a>
+
   <ul class="show">
-<div class="representedDrop">
-    <li><a href="#"> test</a></li><br/>
-    <li><a href="#">test two</a></li><br/>
-</div>
-<div class="worksAvailDrop">
-    <li><a href="#">test 3</a></li><br/>
-    <li><a href="#">test4</a></li><br/>
-</div>
+		<li>
+			<?
+		// assuming artists is the objectType of the artists container
+
+		$artistsContainer =  Graph::object()->objectTypeFilter('artists')->find();
+
+		$artists = $artistsContainer->latticeChildrenQuery()->objectTypeFilter('artist')->publishedFilter()->find_all();?>
+
+		<?if( count($artists) > 0 ):?>
+
+		<ul class="Artists">
+
+			<?foreach($artists as $artist):?>
+
+				<li><a href="<?=latticeurl::site($artist->slug);?>" class="<?echo latticeview::withinSubtree($artist->slug) ? "active" : "";?>" ><?=$artist->title;?></a></li>
+
+			<?endforeach;?>
+
+		</ul>
+
+		<?endif;?>
+
+		<?$wrArtistsContainer =  Graph::object()->objectTypeFilter('worksAvailableArtists')->find();?>
+		
+		<?$wrArtists = $wrArtistsContainer->latticeChildrenQuery()->objectTypeFilter('artist')->publishedFilter()->find_all();?>
+
+		<?if( count($wrArtists) > 0 ):?>
+
+		<ul class="worksAvailableArtists">
+
+			<?foreach($wrArtists as $artist):?>
+
+				<li><a href="<?=latticeurl::site($artist->slug);?>" class="<?echo latticeview::withinSubtree($artist->slug) ? "active" : "";?>" ><?=$artist->title;?></a></li>
+
+			<?endforeach;?>
+
+		</ul>
+		
+ 		<?endif;?>		
+
   </ul>
 </li>
 <li><a href="<?=url::base();?>news"  class="<?echo latticeview::withinSubtree('news') ? "active" : "";?>" >News</a></li>
