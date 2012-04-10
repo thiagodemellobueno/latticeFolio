ffb03961662c303bf3cbff55dd6eff450a3af0b5
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-04-10 16:12:27 -0400

    added markup for preview funcitonality in artists.php and header_publich.php

--- application/views/frontend/artists.php
+++ application/views/frontend/artists.php
@@ -1,16 +1,22 @@
-<div class="container_12">
+<div id="artistListing" class="container_12">
 
-<div id="representedArtists" class="grid_2 push_3" >
-<h3>Artists</h3>
-<?foreach($content['representedArtists'] as $representedArtistsItem):?>
-<a href="<?=url::base('http').$representedArtistsItem['slug'];?>"><?=$representedArtistsItem['title'];?></a><br/>
-<?endforeach;?>
-</div>
+	<div class="preview"></div>
 
-<div id="worksAvailArtists" class="grid_5 push_5">
-<?foreach($content['worksAvailableArtists'] as $worksAvailableArtistsItem):?>
-<a href="<?=url::base('http').$worksAvailableArtistsItem['slug'];?>"><?=$worksAvailableArtistsItem['title'];?></a>
-<?endforeach;?>
-</div>
+	<div id="representedArtists" class="grid_2 prefix_1" >
+	<h3>Artists</h3>
+	<?foreach($content['representedArtists'] as $representedArtistsItem):?>
+		<?$works=Graph::object($representedArtistsItem['slug'])->latticeChildrenQuery()->objectTypeFilter('works')->publishedFilter()->order_by('objectrelationships.sortorder')->find();?>
+		<?$work = $works->latticeChildrenQuery()->objectTypeFilter('work')->publishedFilter()->order_by('objectrelationships.sortorder')->find();?>
+	<a data-previewsrc="<?=$work->image->previewThumb->fullpath;?>" href="<?=url::base('http').$representedArtistsItem['slug'];?>"><?=$representedArtistsItem['title'];?></a><br/>
+	<?endforeach;?>
+	</div>
+
+	<div id="worksAvailArtists" class="grid_5 push_2">
+	<?foreach($content['worksAvailableArtists'] as $worksAvailableArtistsItem):?>
+	<?$works=Graph::object($worksAvailableArtistsItem['slug'])->latticeChildrenQuery()->objectTypeFilter('works')->publishedFilter()->order_by('objectrelationships.sortorder')->find();?>
+	<?$work = $works->latticeChildrenQuery()->objectTypeFilter('work')->publishedFilter()->order_by('objectrelationships.sortorder')->find();?>
+	<a data-previewsrc="<?=$work->image->previewThumb->fullpath;?>" href="<?=url::base('http').$worksAvailableArtistsItem['slug'];?>"><?=$worksAvailableArtistsItem['title'];?></a>
+	<?endforeach;?>
+	</div>
 
 </div>
