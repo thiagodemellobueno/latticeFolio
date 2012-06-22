bf71248832c466bbd8e9b3cd889b950e43cce7a9
Thomas Gillis <thomasgillis@occupywifi.westell.com>
2012-06-22 16:08:25 -0400

    artist name and slug to frontend work view

--- application/views/frontend/work.php
+++ application/views/frontend/work.php
@@ -1,6 +1,31 @@
 <div class="container_12">	
-
 		<?if(is_object($content['main']['image'])):?>
+		  <div class="artistNameAndSlug">
+		    <?php
+		    //start our search for the parent object by loading a lattice object for what we have on the page
+          $artist = Graph::object($content["main"]["id"]);
+
+		      //first, get the id for an 'artist' content type
+ 		      $artist_type_id = ORM::Factory('objecttype')->where('objecttypename','=','artist')->find()->as_array('id');
+          $artist_type_id = $artist_type_id["id"];
+		      //now we ascend the tree until we get to an artist object (or bail at 10 tries)
+          $max = 0;
+          while ($artist->objecttype_id != $artist_type_id && ($max < 10) ) {
+            $max++;
+            $artist= $artist->getLatticeParent();
+          }
+          //if we don't find an artist, the conditional prevents this from being rendered
+		    ?>
+		    <?php if ($artist->objecttype_id ==$artist_type_id):?>
+		    <h2>Artist Name</h2>
+		    <?= $artist->title?>
+		    <h3>Slug</h3>
+		    <?= $artist->slug?>
+		    <?php else:?>
+		      This is an orphan content object, put some message here 
+		    <?php endif?>
+		  </div>
+
 			<img id="image" class="workImage" src="<?=latticeurl::site($content['main']['image']->workImageBig->fullpath);?>" width="<?=$content['main']['image']->workImageBig->width;?>" height="<?=$content['main']['image']->workImageBig->height;?>" alt="<?=$content['main']['title'];?> <?=$content['main']['media'];?> <?=$content['main']['dimensions'];?>" />
 		<?endif;?>
 		
