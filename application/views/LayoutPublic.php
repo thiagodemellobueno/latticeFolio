530a3f053e319ba768099495b98094b118000df4
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-14 11:41:09 -0200

    Updated LayoutPublic to html5, also to printout the pageMeta stuff if config.pageMeta is set to true

--- application/views/LayoutPublic.php
+++ application/views/LayoutPublic.php
@@ -1,19 +1,46 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
-<head>
-	<meta name="viewport" content="width=device-width" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <meta http-equiv="content-script-type" content="text/javascript" />
-	<!-- viewport for mobile browsers -->
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
 
-  <title>J. Cacciola Gallery | <?=latticeview::initialObject()->title;?></title>
-<!-- typkit -->
-<script type="text/javascript" src="http://use.typekit.com/llo3ajs.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width" />
 
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+<!-- pagemeta stuff for seo, involves the pagemeta cluster, and custom objecttype, pagemeta explicitly added to objecttypes in objects.xml -->
+<?if(Kohana::config('site.pageMeta')):?>
+  <link rel="canonical" href="<?='http://'.latticeurl::site( $_SERVER["SERVER_NAME"]);?>" /> 
+  <?$currentURL="http:/".latticeurl::site( $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] );?>
+  <?$home = Graph::object('homepage');?>
+  <?$seoDescription=(latticeview::initialObject()->seo->description )? latticeview::initialObject()->seo->description : $home->seo->description;?>
+  <?$seoTitle=(latticeview::initialObject()->seo->windowTitle )? latticeview::initialObject()->seo->windowTitle : "" ;?>
+  <?$seoKeywords=(latticeview::initialObject()->seo->keywords )? latticeview::initialObject()->seo->keywords : $home->seo->keywords;?>
+  <?$seoThumbSrc=( is_object( latticeview::initialObject()->seo->img->uithumb ) )? latticeview::initialObject()->seo->img->uithumb->fullpath : NULL;?>  
+  <meta property="og:title" content=" <?=Kohana::config('siteTitle');?> <?if($seoTitle):?>| <?=$seoTitle?>"<?endif;?> />
+  <meta name="description" content="<?=$seoDescription;?>" /> 
+  <meta name="keywords" content="<?=$seoKeywords;?>" />
+  <meta property="og:url" content="<?=$currentURL;?>">
+  <meta property="og:description" content="<?=$seoDescription;?>" />
+  <?if($seoThumbSrc):?>
+    <link rel="image_src" type="image/jpeg" href="<?=$seoThumbSrc;?>" />
+    <meta property="og:image" content="<?=$seoThumbSrc;?>" />
+  <?endif;?>
+<?endif;?>
+
+<title><?=Kohana::config('site.title');?> <?=($seoTitle)?$seoTitle:latticeview::initialObject()->title;?></title>
+
+    <!-- html 5 boilerplate -->
+    <link rel="stylesheet" href="resources/css/normalize.css">
+<!--
+    <link rel="stylesheet" href="resources/css/main.css">
+    <script src="resources/js/vendor/modernizr-2.6.2.min.js"></script>
+-->
   <?=$stylesheet;?>
-	<?=$javascript;?>
+  <?=$javascript;?>
 
 </head>
 
