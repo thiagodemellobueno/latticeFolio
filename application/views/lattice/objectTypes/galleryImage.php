dd3f0881917dc70ac2fb76ed6673b722d0a63010
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-14 11:19:18 -0200

    galleryImage custom objecttype

--- application/views/lattice/objectTypes/galleryImage.php
+++ application/views/lattice/objectTypes/galleryImage.php
@@ -1,14 +1,21 @@
-<?$instance=$data['instance'];?>
-<li data-objectid="<?=$data['id'];?>" id="item_<?=$data['id'];?>" class="listItem grid_4"> 
+<li data-objectid="<?=$data['id'];?>" id="item_<?=$data['id'];?>" class="galleryImage listItem grid_4 <?=latticeutil::modulo( "gallerypanels", array( "alpha","","omega" ));?>">
 
 	<?=$image_image;?>
-	<?=$text_header;?>
-	<?=$text_description;?>
 
-	<div class="itemControls clearFix">
+	<div class='modalContent hidden'>
+		<div class="clearFix">
+			<?=$tags_tags;?>
+		</div>
+		<?=$text_imageTitle;?>
+		<?=$text_blurb;?>
+		<?=$link_link;?>
+	</div>
+
+	<div class="itemControls">
+		<a href="#" title="Edit metadata for this item:" class="icon meta modal-actuator">meta</a>
 		<a href="#" title="delete this list item" class="icon delete">delete</a>
 		<a href="#" title="Add This Item" class="button submit hidden">submit</a>
 		<a href="#" title="Cancel" class="button cancel hidden">cancel</a>
 	</div>
-
+		
 </li>
