2e6828394f98309fb29c8e20718cb6e6a111665f
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-14 11:19:44 -0200

    header and footer more semantic and html5-y

--- application/views/footer_public.php
+++ application/views/footer_public.php
@@ -1,49 +1,5 @@
-<div id="footer">
+<footer id="footer">
 
-	<div class="container_12 museo">
-		<div class="grid_3 alpha">
-			<ul id="contact">
-				<li>J. Cacciola Gallery</li>
-				<li><a href="mailto:info@jcacciolagallery.com">info@jcacciolagallery.com</a></li>
-			</ul>
-		</div>
-		<div class="grid_3">
-			<address>
-				537 West 23rd Street <br/>
-				New York, NY 10011
-			</address>
-		</div>
-		<div class="grid_3">
-			<ul id="tel">
-				<li>p.212.462.4646</li>
-				<li>f.212.462.4556</li>
-			</ul>
-		</div>
-		<div class="grid_3 omega">
-			<ul id="social">
-				<li>
-					<a target="_blank" rel="nofollow" href="http://www.facebook.com/pages/JCacciola-Gallery/132999769923">
-						<img src="<?=url::base();?>application/resources/images/facebook.png" alt="facebook logo" />
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="http://twitter.com/cacciolagallery">
-						<img src="<?=url::base();?>application/resources/images/twitter.png" alt="twitter logo"/>
-					</a>
-				</li>
-			</ul>
-		</div>
-	</div>
 
-</div>
+</footer>
 
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-29042751-1``']);
-	_gaq.push(['_trackPageview']);
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
--- application/views/header_public.php
+++ application/views/header_public.php
@@ -1,25 +1,19 @@
-<div class="container_12">
+<header>
 
-	<div class="header clearfix">
+	<div class="header">
 
-		<h1 id="logo"><a href="<?=url::base();?>home"><img src="application/resources/images/j-cacciola-gallery-logo.png" width="451" height="46" alt="J. Cacciola Gallery" /></a></h1>
+		<div class="branding">
+
+			<h1 id="logo"><a href="<?=url::base();?>homepage"></a></h1>
+			<h1 id="site-name"></h1>
 		
-		<ul id="nav" class="grid_5 push_1 omega">
-
-			<li id="artistsNav" >
-				<a href="<?=url::base();?>artists"  class="<?echo latticeview::withinSubtree('artists') ? "active" : "";?>" >Artists</a>
-			</li>
-
-			<li><a href="<?=url::base();?>news" class="<?echo latticeview::withinSubtree('News') ? "active" : "";?>" >News</a></li>
-			<li><a href="<?=url::base();?>press" class="<?echo latticeview::withinSubtree('press') ? "active" : "";?>" >Press</a></li>
-			<li><a href="<?=url::base();?>exhibitions" class="<?echo latticeview::withinSubtree('exhibitions') ? "active" : "";?>" >Exhibitions</a></li>
-			<?php try {?>
-			<li><a href="<?=url::base();?>about" class="<?php  echo latticeview::withinSubtree('about') ? "active" : "";?>" >About</a></li>
-      <?php }  catch (Exception $e) {
-        
-      }?>
-		</ul>
-	</div>
+			<nav>
+				<ul id="nav">
+					
+				</ul>
+			</nav>
+	
+		</div>
 
 	
 </div>
